ada15fd74cc496459eccbe1492f8855780e865b0
andreinst <nistoreanu_andrei@yahoo.com>
2023-10-27 18:52:18 +0300

    AddedFeatures

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print('Hello World')
 
-print('In the end I managed it to create 2 files')
+print('In the end@@@ I managed it to create 2 files')
+
+print ('another commit')
+
+print ('New Branch2 is seted')
